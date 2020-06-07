387c1b3fc97e4d714a364d24fd2c07b1567890fc
Icaro C. Capobianco <icaro.capobianco@gmail.com>
2020-06-07 19:48:15 -0300

    Updated Custom Fields Trait

--- Traits/CustomFieldsTrait.php
+++ Traits/CustomFieldsTrait.php
@@ -7,15 +7,15 @@ trait CustomFieldsTrait {
 
     abstract function get_acf_id();
 
-    public function get_field( $field_name, $fallback = 'fallback', $format = true, $updated = false ) {
-		if ( ! isset( $this->acf[ $field_name ] ) && ! $updated ) {
+	public function get_field( string $field_name, $fallback = -999, $format = true ) {
+		if ( ! isset( $this->acf[ $field_name ] ) ) {
 			$field_value = get_field( $field_name, $this->get_acf_id(), $format );
-			if ( ! $field_value && $fallback !== 'fallback' ) {
+			if ( ! $field_value && $fallback !== -999 ) {
 				$field_value = $fallback;
 			}
 			$this->acf[ $field_name ] = $field_value;
-        }
+		}
 		return $this->acf[ $field_name ];
-    }
+	}
 
 }
