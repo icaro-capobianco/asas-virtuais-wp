1e10371232dafc0c9c280789e31459494e1f804f
Icaro C. Capobianco <icaro.capobianco@gmail.com>
2020-06-17 18:05:09 -0300

    Making sure no function duplicate is made

--- lib/Debug.php
+++ lib/Debug.php
@@ -1,15 +1,16 @@
 <?php
 
-function av_show( $var, $dump = false, $exit = true ) {
-	echo '<pre>';
-	if ( $dump ) {
-		var_dump( $var );
-	} else {
-		print_r( $var );
-	}
-	echo '</pre>';
-	if ( $exit ) {
-		exit();
+if ( ! function_exists( 'av_show' ) ) {
+	function av_show( $var, $dump = false, $exit = true ) {
+		echo '<pre>';
+		if ( $dump ) {
+			var_dump( $var );
+		} else {
+			print_r( $var );
+		}
+		echo '</pre>';
+		if ( $exit ) {
+			exit();
+		}
 	}
 }
-
--- lib/Media.php
+++ lib/Media.php
@@ -1,12 +1,13 @@
 <?php
-
-function av_attachment_index( $filename ) {
-	global $wpdb;
-	$title   = sanitize_file_name( pathinfo( $filename, PATHINFO_FILENAME ) );
-	$query   = "SELECT ID FROM $wpdb->posts WHERE post_type = 'attachment' AND ( post_title LIKE '%$title%' OR guid LIKE '%$filename'  )";
-	$results = $wpdb->get_results( $query );
-	if ( ! empty( $results ) ) {
-		return $results[0];
+if ( ! function_exists( 'av_attachment_index' ) ) {
+	function av_attachment_index( $filename ) {
+		global $wpdb;
+		$title   = sanitize_file_name( pathinfo( $filename, PATHINFO_FILENAME ) );
+		$query   = "SELECT ID FROM $wpdb->posts WHERE post_type = 'attachment' AND ( post_title LIKE '%$title%' OR guid LIKE '%$filename'  )";
+		$results = $wpdb->get_results( $query );
+		if ( ! empty( $results ) ) {
+			return $results[0];
+		}
+		return false;
 	}
-	return false;
 }
