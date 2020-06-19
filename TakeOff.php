76e26c4f5615d0cf9a513604a7c8840cf87ae70b
Icaro C. Capobianco <icaro.capobianco@gmail.com>
2020-06-19 15:00:38 -0300

    3.0.1

--- TakeOff.php
+++ TakeOff.php
@@ -57,4 +57,4 @@ if ( ! class_exists( '\AsasVirtuais\WP\Framework\TakeOff' ) ) {
 	}
 }
 
-return TakeOff::instance()->register_version( __FILE__, '3.0.0' );
+return TakeOff::instance()->register_version( __FILE__, '3.0.1' );
