096456baf671f4fa1fe3b5b5328a68666e2df963
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2020-12-16 13:05:01 -0300

    6.8.1 fix unchecked function

--- lib/Plugins.php
+++ lib/Plugins.php
@@ -2,9 +2,11 @@
 
 defined( 'ABSPATH' ) or exit;
 
-function av_get_plugin_data( $plugin_file ) {
-	if( ! function_exists('get_plugin_data') ){
-		require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+if ( ! function_exists( 'av_get_plugin_data' ) ) {
+	function av_get_plugin_data( $plugin_file ) {
+		if( ! function_exists('get_plugin_data') ){
+			require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		}
+		return get_plugin_data( $plugin_file, false );
 	}
-	return get_plugin_data( $plugin_file, false );
 }
